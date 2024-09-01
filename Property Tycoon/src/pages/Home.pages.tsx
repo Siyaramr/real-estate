2165b6b5ecac31672e97329ea67ba63a65a93946
Siyaramr <ranasiyaram6194@gmail.com>
2024-09-02 02:35:41 +0545

    first commit

---
+++ Property Tycoon/src/pages/Home.pages.tsx
@@ -0,0 +1,10 @@
+import HeaderPage from "../components.tsx/header.components";
+const HomePage = () => {
+    return(
+        <>
+        <HeaderPage/>
+        <h1>This is a Home page</h1>
+        </>
+    )
+}
+export default HomePage;
